038d5492d49544ac3b90c619f776dbdb74cc5849
at01071988 <39896583+at01071988@users.noreply.github.com>
2020-10-28 16:42:49 +0530

    Add files via upload

---
+++ eurekaServerExample/src/test/java/com/example/eurekaServerExample/EurekaServerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekaServerExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
